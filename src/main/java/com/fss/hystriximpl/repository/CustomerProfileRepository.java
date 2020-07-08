8684e977b0c3a864d151a275e5f6d31b61a2f1a8
jstaadi <adityasai102@gmail.com>
2020-07-08 12:53:05 +0530

    Revert "Hystrix Implementation"
    This reverts commit ab33da46

--- src/main/java/com/fss/hystriximpl/repository/CustomerProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fss.hystriximpl.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CustomerProfileRepository {
-
-}
--- src/main/java/com/fss/hystriximpl/service/AccountDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.fss.hystriximpl.service;
-
-import com.fss.hystriximpl.domain.AccountDetails;
-
-public interface AccountDetailsService {
-
-    public AccountDetails getDetails(String accountId);
-}
